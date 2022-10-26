3b70874fa0ebf6d8ea3bf872e5b93df73f646b46
Paul Barber <paul_j_barber@yahoo.co.uk>
2022-10-26 21:35:29 +0100

    Output basic NI ROI maps as HTML

--- process.py
+++ process.py
@@ -1,28 +1,16 @@
-from genericpath import isfile
 import geopandas as gpd
-from shapely.geometry import Polygon
+from shapely.geometry import *
 import numpy as np
 import topojson as tp
 import met_brewer
 import requests
 import os.path
-from selenium import webdriver
 import logging
 import argparse
 import altair as alt
-alt.data_transformers.disable_max_rows()
+from altair_saver import save
 
-def extract_coord_lists(x):
-    if x.type == 'MultiLineString':
-#        return list([(y[0],y[1]) for y in x[0].coords])
-        if len(x.geoms) == 1:
-            return list([(y[0],y[1]) for y in x.geoms[0].coords])
-        else:
-            return [list([(y[0],y[1]) for y in line.coords]) for line in x.geoms]
-    elif x.type == 'LineString':
-        return list([(y[0],y[1]) for y in x.coords])
-    else:
-        raise Exception('Unknown type {x.type}')
+alt.data_transformers.disable_max_rows()
 
 def download_file_if_not_exists(url, fname=None):
     if fname is None:
@@ -35,57 +23,78 @@ def download_file_if_not_exists(url, fname=None):
                 for chunk in stream.iter_content(chunk_size=8192): 
                     f.write(chunk)
 
-def get_chrome_driver():
-    options = webdriver.ChromeOptions()
-    options.headless = True
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument("--window-size=1280,720")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--hide-scrollbars")
-    options.add_argument("--disable-infobars")
-    options.add_argument("--enable-logging")
-    options.add_argument("--log-level=0")
-    options.add_argument("--v=99")
-    options.add_argument("--verbose")
-    options.add_argument("--single-process")
-    options.add_argument("--user-data-dir=/tmp/user-data/")
-    options.add_argument("--data-path=/tmp/data/")
-    options.add_argument("--homedir=/tmp/homedir/")
-    options.add_argument("--disk-cache-dir=/tmp/disk-cache/")
-    options.add_argument("--disable-async-dns")
-    options.add_argument("--remote-debugging-port=9222")
-    options.add_argument("--log-path=/tmp/chromium.log")
-    options.add_argument("--autoplay-policy=no-user-gesture-required")
-    options.add_argument("--no-first-run")
-    options.add_argument("--disable-sync")
-    driver = None
-    for attempt in range(3):
-        try:
-            driver = webdriver.Chrome(service_log_path='/tmp/chromedriver.log', options=options)
-        except:
-            logging.exception('Failed to setup chromium')
-            if os.path.isfile('/tmp/chromedriver.log'):
-                with open('/tmp/chromedriver.log') as log:
-                    logging.warning(log.read())
-            logging.error([f for f in os.listdir('/tmp/')])
-        else:
-            break
+# From https://gis.stackexchange.com/a/220374
+def remove_third_dimension(geom):
+    if geom.is_empty:
+        return geom
+
+    if isinstance(geom, Polygon):
+        exterior = geom.exterior
+        new_exterior = remove_third_dimension(exterior)
+
+        interiors = geom.interiors
+        new_interiors = []
+        for int in interiors:
+            new_interiors.append(remove_third_dimension(int))
+
+        return Polygon(new_exterior, new_interiors)
+
+    elif isinstance(geom, LinearRing):
+        return LinearRing([xy[0:2] for xy in list(geom.coords)])
+
+    elif isinstance(geom, LineString):
+        return LineString([xy[0:2] for xy in list(geom.coords)])
+
+    elif isinstance(geom, Point):
+        return Point([xy[0:2] for xy in list(geom.coords)])
+
+    elif isinstance(geom, MultiPoint):
+        points = list(geom.geoms)
+        new_points = []
+        for point in points:
+            new_points.append(remove_third_dimension(point))
+
+        return MultiPoint(new_points)
+
+    elif isinstance(geom, MultiLineString):
+        lines = list(geom.geoms)
+        new_lines = []
+        for line in lines:
+            new_lines.append(remove_third_dimension(line))
+
+        return MultiLineString(new_lines)
+
+    elif isinstance(geom, MultiPolygon):
+        pols = list(geom.geoms)
+
+        new_pols = []
+        for pol in pols:
+            new_pols.append(remove_third_dimension(pol))
+
+        return MultiPolygon(new_pols)
+
+    elif isinstance(geom, GeometryCollection):
+        geoms = list(geom.geoms)
+
+        new_geoms = []
+        for geom in geoms:
+            new_geoms.append(remove_third_dimension(geom))
+
+        return GeometryCollection(new_geoms)
+
     else:
-        logging.error('Failed to set up webdriver after %d attempts' %(attempt+1))
-    return driver
+        raise RuntimeError("Currently this type of geometry is not supported: {}".format(type(geom)))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create river map of the island of Ireland.')
     parser.add_argument('--colours', help='RMetBrewer colour scheme', default='Derain')
+    parser.add_argument('--maps', help='Choose maps to create', nargs='+', default=['Hydro', 'NI', 'ROI'])
     args = parser.parse_args()
 
-    driver = get_chrome_driver()
+    # Colour schemes from RMetBrewer
+    colours = [(int(c[1:3], 16), int(c[3:5], 16), int(c[5:], 16)) for c in met_brewer.met_brew(args.colours)]
 
-    if driver is not None:
-        # Colour schemes from RMetBrewer
-        colours = [(int(c[1:3], 16), int(c[3:5], 16), int(c[5:], 16)) for c in met_brewer.met_brew(args.colours)]
-
+    if 'Hydro' in args.maps:
         # Get Hydrorivers data for Ireland and cut off the Scotland area of the bounding box
         download_file_if_not_exists('https://data.hydrosheds.org/file/HydroRIVERS/HydroRIVERS_v10_eu.gdb.zip')
         eu = gpd.read_file('HydroRIVERS_v10_eu.gdb.zip', bbox=(-10.56,51.39,-5.34,55.43))
@@ -96,7 +105,6 @@ if __name__ == '__main__':
             (-5.34, 55.23)
             ]
         ))]
-        eu['plotstrings'] = eu.geometry.apply(extract_coord_lists) # Convert to pydeck friendly format
         download_file_if_not_exists('https://data.hydrosheds.org/file/hydrobasins/standard/hybas_eu_lev01-12_v1c.zip')
         # Get Hydrobasins data for Ireland and apply colours
         eubas = gpd.read_file('hybas_eu_lev01-12_v1c.zip', layer='hybas_eu_lev06_v1c', bbox=(-10.56,51.39,-5.34,55.43))
@@ -126,4 +134,58 @@ if __name__ == '__main__':
             width = 1000
         )
 
-        lines.save('test.png', format='png', method='selenium', webdriver=driver)
+        save(lines, 'hydrorivers_hydrobasins.html', format='html')
+
+    if 'NI' in args.maps:
+        download_file_if_not_exists('https://opendata-daerani.hub.arcgis.com/datasets/DAERANI::rivers-strahler-ranking.zip?outSR=%7B%22latestWkid%22%3A29902%2C%22wkid%22%3A29900%7D', 'ni-rivers-strahler-ranking.zip')
+        gdf = gpd.read_file('ni-rivers-strahler-ranking.zip')
+        gdf.geometry = gdf.geometry.to_crs('4326')
+        gdf['linewidth'] = gdf.strahler / 3
+
+        lines = alt.Chart(gdf).mark_geoshape(
+            filled=False,
+        ).encode(
+            strokeWidth=alt.StrokeWidth(
+                "linewidth",
+                legend=None
+            ),
+#            color=alt.Color(
+#                "hexcolour", 
+#                scale=None
+#            )
+        ).properties(
+            height = 1300,
+            width = 1000
+        )
+
+        save(lines, 'ni_rivers.html', format='html')
+
+    if 'ROI' in args.maps:
+        download_file_if_not_exists('http://gis.epa.ie/geoserver/EPA/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=EPA:WATER_RIVNETROUTES&outputFormat=application%2Fjson&srsName=EPSG:4326', 'roi-river-netroutes.json')
+        ie = gpd.read_file('roi-river-netroutes.json')
+        ie.geometry = ie.geometry.to_crs('4326')
+        ie.geometry = ie.geometry.apply(lambda x: remove_third_dimension(x))
+        topo = tp.Topology(ie, prequantize=False)
+        roisimplify = topo.toposimplify(0.01).to_gdf()
+        roisimplify = roisimplify.set_crs('EPSG:4326')
+        roisimplify['linewidth'] = roisimplify.ORDER_ / 3
+
+        lines = alt.Chart(roisimplify).mark_geoshape(
+            filled=False,
+        ).encode(
+            strokeWidth=alt.StrokeWidth(
+                "linewidth",
+                legend=None
+            ),
+#            color=alt.Color(
+#                "hexcolour", 
+#                scale=None
+#            )
+        ).properties(
+            height = 1300,
+            width = 1000
+        )
+
+        print('Created plot')
+
+        save(lines, 'ie_rivers.html', format='html')
